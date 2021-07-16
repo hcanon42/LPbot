edaca48f3c0afe92d050744e250fa118042a7362
hcanon <hcanon@student.42.fr>
2021-07-16 11:26:46 +0200

    button click

--- example.js
+++ example.js
@@ -4,6 +4,7 @@ const puppeteer = require('puppeteer');
 require('dotenv').config({path:"./.env"});
 
 const clipeee = {x: 0, y: 0, width: 810, height: 680}
+
 client.on("ready", () =>
 {
   console.log(`Logged in as ${client.user.tag}!`)
@@ -19,10 +20,12 @@ client.on("message", msg =>
 		const page = await browser.newPage();
 		await page.goto('https://zeal.gg/wnDrPTx');
 		await page.waitForTimeout(1000);
+		await page.mouse.click(630, 103)
+		await page.waitForTimeout(2000);
 		await page.screenshot({ type: "jpeg", path: 'screen.jpg', quality: 100, clip:clipeee});
 		await browser.close();
 	  })();
-	  msg.channel.send({files:['./screen.jpg']});
+	msg.channel.send({files:['./screen.jpg']});
     //alert(screenshotTarget);
   }
 })
